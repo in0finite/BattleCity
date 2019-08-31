214229f72c7f4b66673703f5556df241dd73d8bc
in0finite <leshow816@gmail.com>
2019-09-01 01:13:30 +0200

    limit fps

--- Assets/BattleCity/Scripts/GameManager.cs
+++ Assets/BattleCity/Scripts/GameManager.cs
@@ -9,8 +9,19 @@ namespace BattleCity
 
 	    void Start()
 	    {
-	        
+			
+	        if (Application.isEditor)
+				SetMaxFps(15);
+			else
+				SetMaxFps(30);
+			
 	    }
+
+		static void SetMaxFps(int maxFps)
+		{
+			QualitySettings.vSyncCount = 0;
+			Application.targetFrameRate = maxFps;
+		}
 
 	    void Update()
 	    {
