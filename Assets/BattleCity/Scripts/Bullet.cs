0d05d6769e65953cc83b6f9cbe6bd8516bc0d0c1
in0finite <leshow816@gmail.com>
2019-09-10 03:51:30 +0200

    cache bullets

--- Assets/BattleCity/Scripts/Bullet.cs
+++ Assets/BattleCity/Scripts/Bullet.cs
@@ -6,6 +6,9 @@ namespace BattleCity
 	
 	public class Bullet : MonoBehaviour
 	{
+		static List<Bullet> s_bullets = new List<Bullet>();
+		public static List<Bullet> AllBullets => s_bullets;
+
 		public Tank TankShooter { get; set; }
 		public float damage = 50f;
 		bool m_collided = false;
@@ -15,6 +18,16 @@ namespace BattleCity
 		void Awake()
 		{
 			Debug.LogFormat("Bullet.Awake()");
+		}
+
+		void OnEnable()
+		{
+			s_bullets.Add(this);
+		}
+
+		void OnDisable()
+		{
+			s_bullets.Remove(this);
 		}
 		
 		void Start()
